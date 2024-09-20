4ba1c738fa7c07fa3b1a2e8aa48ed4b23bf817ce
Nadinnnka <tymoshyk02@gmail.com>
2024-09-20 13:21:12 +0300

    make build

---
+++ index.2dce99a4.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("#largeImg");document.querySelector("#thumbs").addEventListener("click",function(t){t.preventDefault();var r=t.target.closest("a");r&&(e.src=r.href,e.alt=r.title)});
+//# sourceMappingURL=index.2dce99a4.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,12 @@
+"use strict";
+const mainImg = document.querySelector("#largeImg");
+const pictures = document.querySelector("#thumbs");
+pictures.addEventListener("click", function(e) {
+    e.preventDefault();
+    const target = e.target.closest("a");
+    if (!target) return;
+    mainImg.src = target.href;
+    mainImg.alt = target.title;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
